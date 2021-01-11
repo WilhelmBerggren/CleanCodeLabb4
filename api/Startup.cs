b026529bbf7dc1c3fa86c1ef17bce6a317af245e
WilhelmBerggren <wilhelmberggren@hotmail.com>
2021-01-11 11:58:59 +0100

    Started containerizing

--- api/Startup.cs
+++ api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,12 @@ namespace api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            // services.AddDbContext<ApiDbContext>(options =>
+            //     options.UseNpgsql(
+            //         connectionString
+            //     )
+            // );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
