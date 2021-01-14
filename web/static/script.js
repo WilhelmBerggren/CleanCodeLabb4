a33d2a6b23443891fa8747cb67e2f1bb2280c19b
WilhelmBerggren <wilhelmberggren@hotmail.com>
2021-01-14 13:50:57 +0100

    Cleaned up frontend

--- web/static/script.js
+++ web/static/script.js
@@ -2,16 +2,29 @@ import { html, render, useState, useEffect } from './libs/htm.preact.standalone.
 
 const api = "http://localhost:5000/api";
 
+function SubmitForm({ submitPost }) {
+  return html`
+    <p>New post: 
+      <form onsubmit=${submitPost}>
+        <input name="title" placeholder="title" />
+        <input name="url" placeholder="url" />
+        <input type="submit" value="yeet it to the cloud"/>
+      </form>
+    </p>`;
+}
+
 function Post({post, deletePost}) {
+  const { id, title, url } = post;
+
   return html`
     <div class="post">
-      <p><strong>${post.id}: ${post.title}</strong></p>
-      <a href=${post.url}>
-        ${post.url.endsWith('jpg') 
-          ? html`<img loading="lazy" width="300" src=${post.url} alt="post image" />` 
-          : post.url}
+      <p><strong>${id}: ${title}</strong></p>
+      <a href=${url}>
+        ${url.endsWith('jpg') 
+          ? html`<img loading="lazy" width="300" src=${url} alt="post image" />` 
+          : url}
       </a>
-      <p><button onclick="${() => deletePost(post.id)}"></button></p>
+      <p><button onclick="${() => deletePost(id)}">Delete post</button></p>
     </div>
   `;
 }
@@ -26,14 +39,6 @@ function usePosts() {
       .then(d => setPosts(d || []));
   }
 
-  function deletePost(id) {
-    fetch(`${api}/posts/${id}`, {
-      method: "DELETE",
-      mode: "cors",
-    })
-    .then(fetchPosts);
-  }
-
   function submitPost() {
     e.preventDefault();
     const data = new FormData(e.target);
@@ -44,6 +49,14 @@ function usePosts() {
       mode: "cors",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({ url, title })
+    })
+    .then(fetchPosts);
+  }
+
+  function deletePost(id) {
+    fetch(`${api}/posts/${id}`, {
+      method: "DELETE",
+      mode: "cors",
     })
     .then(fetchPosts);
   }
@@ -62,13 +75,7 @@ function App() {
   return html`
     <div class="body">
       <p class="title">Nilhelm Hacker News [48 65 6a 20 44 61 76 69 64]</p>
-      <p>New post: 
-        <form onsubmit=${submitPost}>
-          <input name="title" placeholder="title" />
-          <input name="url" placeholder="url" />
-          <input type="submit" value="yeet it to the cloud"/>
-        </form>
-      </p>
+      <${SubmitForm} submitPost=${submitPost} />
       <div>
         ${posts.map(post => html`
           <${Post} key=${post.id} post=${post} deletePost=${deletePost} />
